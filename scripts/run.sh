20ac290f9a201c93a9ae1dbe5b94da359dee5848
Huan <zixia@zixia.net>
2020-12-28 09:26:20 +0000

    add dev run script

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+docker run \
+  --rm \
+  -ti \
+  -e SALSA17_MYSQL_HOST \
+  -e SALSA17_MYSQL_USER \
+  -e SALSA17_MYSQL_PASS \
+  -e SALSA17_MYSQL_DATABASE \
+  -v /data/17salsa.com/home/data:/www/home/data \
+  -v /data/17salsa.com/home/attachment:/www/home/attachment \
+  -v /data/17salsa.com/center/data:/www/center/data \
+  -p 8888:80 \
+  --entrypoint bash \
+  ghcr.io/zixia/17salsa.com
