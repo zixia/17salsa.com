5c9c4355108e8002f334a7187dc35898ec3639f8
Huan <zixia@zixia.net>
2020-12-28 06:34:03 +0000

    17salsa.com/fm radio

---
+++ www/fm/17salsa_radio_files/axurerp_pagespecificscript.js
@@ -0,0 +1,295 @@
+
+var PageName = '17salsa_radio';
+var PageId = 'pc14dee115bbb47429ae47ed76c2bfb1e'
+var PageUrl = '17salsa_radio.html'
+document.title = '17salsa_radio';
+
+if (top.location != self.location)
+{
+	if (parent.HandleMainFrameChanged) {
+		parent.HandleMainFrameChanged();
+	}
+}
+
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  return value;
+}
+
+function OnLoad(e) {
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=111676&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+eval(GetDynamicPanelScript('u5', 2));
+
+var u16 = document.getElementById('u16');
+
+u16.style.cursor = 'pointer';
+if (bIE) u16.attachEvent("onclick", Clicku16);
+else u16.addEventListener("click", Clicku16, true);
+function Clicku16(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=108717&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'center';
+var u15 = document.getElementById('u15');
+gv_vAlignTable['u15'] = 'center';
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'center';
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'center';
+var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'center';
+var u22 = document.getElementById('u22');
+
+u22.style.cursor = 'pointer';
+if (bIE) u22.attachEvent("onclick", Clicku22);
+else u22.addEventListener("click", Clicku22, true);
+function Clicku22(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=111676&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u12 = document.getElementById('u12');
+
+u12.style.cursor = 'pointer';
+if (bIE) u12.attachEvent("onclick", Clicku12);
+else u12.addEventListener("click", Clicku12, true);
+function Clicku12(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=108725&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u5 = document.getElementById('u5');
+
+var u8 = document.getElementById('u8');
+
+u8.style.cursor = 'pointer';
+if (bIE) u8.attachEvent("onclick", Clicku8);
+else u8.addEventListener("click", Clicku8, true);
+function Clicku8(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="about:blank" + "";
+
+	SetPanelStateu5("pd0u5");
+
+}
+
+}
+
+var u10 = document.getElementById('u10');
+
+u10.style.cursor = 'pointer';
+if (bIE) u10.attachEvent("onclick", Clicku10);
+else u10.addEventListener("click", Clicku10, true);
+function Clicku10(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=22036&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u0 = document.getElementById('u0');
+
+var u25 = document.getElementById('u25');
+gv_vAlignTable['u25'] = 'center';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'center';
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'center';
+var u3 = document.getElementById('u3');
+
+u3.style.cursor = 'pointer';
+if (bIE) u3.attachEvent("onclick", Clicku3);
+else u3.addEventListener("click", Clicku3, true);
+function Clicku3(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=108722&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u23 = document.getElementById('u23');
+gv_vAlignTable['u23'] = 'center';
+var u14 = document.getElementById('u14');
+
+u14.style.cursor = 'pointer';
+if (bIE) u14.attachEvent("onclick", Clicku14);
+else u14.addEventListener("click", Clicku14, true);
+function Clicku14(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=102276&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u6 = document.getElementById('u6');
+
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'center';
+var u20 = document.getElementById('u20');
+
+u20.style.cursor = 'pointer';
+if (bIE) u20.attachEvent("onclick", Clicku20);
+else u20.addEventListener("click", Clicku20, true);
+function Clicku20(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=125061&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u1 = document.getElementById('u1');
+
+u1.style.cursor = 'pointer';
+if (bIE) u1.attachEvent("onclick", Clicku1);
+else u1.addEventListener("click", Clicku1, true);
+function Clicku1(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=115231&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u11 = document.getElementById('u11');
+gv_vAlignTable['u11'] = 'center';
+var u18 = document.getElementById('u18');
+
+u18.style.cursor = 'pointer';
+if (bIE) u18.attachEvent("onclick", Clicku18);
+else u18.addEventListener("click", Clicku18, true);
+function Clicku18(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=110847&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u24 = document.getElementById('u24');
+
+u24.style.cursor = 'pointer';
+if (bIE) u24.attachEvent("onclick", Clicku24);
+else u24.addEventListener("click", Clicku24, true);
+function Clicku24(e)
+{
+
+if (true) {
+
+	objIframe = document.getElementById("u0");
+	objIframe.src="http://radiotime.com/WebTuner.aspx?StationId=113759&" + "";
+
+	SetPanelStateu5("pd1u5");
+
+}
+
+}
+
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'center';
+if (window.OnLoad) OnLoad();
