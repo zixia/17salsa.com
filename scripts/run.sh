045a50b9d4de0cbd21979de582ca25fe0bcaaab3
Huan LI (李卓桓) <zixia@zixia.net>
2021-01-08 02:41:28 +0800

    follow /var/www path convension

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,11 +9,11 @@ docker run \
   -e SALSA17_MYSQL_HOST \
   -e SALSA17_MYSQL_USER \
   -e SALSA17_MYSQL_PASS \
-  -v /data/17salsa.com/home/data:/webroot/home/data \
-  -v /data/17salsa.com/home/attachment:/webroot/home/attachment \
-  -v /data/17salsa.com/center/data:/webroot/center/data \
-  -v /data/17salsa.com/wiki/data:/webroot/wiki/data \
-  -v /data/17salsa.com/wiki/uploads:/webroot/wiki/uploads \
+  -v /data/17salsa.com/home/data:/var/www/home/data \
+  -v /data/17salsa.com/home/attachment:/var/www/home/attachment \
+  -v /data/17salsa.com/center/data:/var/www/center/data \
+  -v /data/17salsa.com/wiki/data:/var/www/wiki/data \
+  -v /data/17salsa.com/wiki/uploads:/var/www/wiki/uploads \
   -p 8080:80 \
   -e VIRTUAL_HOST=17salsa.com,*.17salsa.com,17salsa.net,*.17salsa.net,17salsa.org,*.17salsa.org \
   -e LETSENCRYPT_HOST=17salsa.com,www.17salsa.com,cdn.17salsa.com,abu.17salsa.com,17salsa.net,www.17salsa.net,17salsa.org,www.17salsa.org \
